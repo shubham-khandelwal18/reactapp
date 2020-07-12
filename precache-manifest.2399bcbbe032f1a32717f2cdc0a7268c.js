032f0b44ed74597ef47937de6fe514a896ed3c11
kshitiz kamal <skilite007@gmail.com>
2020-07-12 14:30:21 +0530

    Updates

---
+++ precache-manifest.2399bcbbe032f1a32717f2cdc0a7268c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4316c1e4fc5c8c7882e966a7efe85b83",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "3175f289881b29651723",
+    "url": "/reactapp/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "5429362f0458f548cc12",
+    "url": "/reactapp/static/css/main.9f7e0114.chunk.css"
+  },
+  {
+    "revision": "3175f289881b29651723",
+    "url": "/reactapp/static/js/2.b2757867.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactapp/static/js/2.b2757867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5429362f0458f548cc12",
+    "url": "/reactapp/static/js/main.3be0e9bd.chunk.js"
+  },
+  {
+    "revision": "284241a5c4b31735a643",
+    "url": "/reactapp/static/js/runtime-main.4a5aa62f.js"
+  }
+]);
